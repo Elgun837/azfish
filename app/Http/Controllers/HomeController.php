c4fe4dd26deff3122d8de7ddff97cb84776c2d57
Kamran <kamran9909@mail.ru>
2022-12-07 17:23:07 +0400

    home route test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,6 +17,8 @@ class HomeController extends Controller
     public function index()
     {
 
-        return view('home');
+        $slider = Slider::all();
+
+        return response()->json($slider, 200);
     }
 }
