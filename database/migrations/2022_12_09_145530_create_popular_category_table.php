e02f17df84dbcb73819d23010fac3b76d3d47ccf
Kamran <kamran9909@mail.ru>
2022-12-09 15:09:28 +0400

    add custom field and create first module

---
+++ database/migrations/2022_12_09_145530_create_popular_category_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePopulaCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('popular_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->json('categories')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('popular_category');
+    }
+}
