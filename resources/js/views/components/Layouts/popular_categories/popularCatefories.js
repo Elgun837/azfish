f910e742169430075321dd331936619bed7c0e0e
elgun837 <elgun.xalilov@bk.ru>
2022-12-16 14:04:57 +0400

    p-cat-api

--- resources/js/views/components/Layouts/popular_categories/popularCatefories.js
+++ resources/js/views/components/Layouts/popular_categories/popularCatefories.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom'
 
 export default function popularCatefories() {
     return (
-        <div className="pop_cat-section">
-            <div className='header-box justify-content-center'>
+        <div className="pop_cat-section container">
+           <div className='header-column text-center'>
+           <div className='header-box justify-content-center'>
                 <img className='svg-img' src={fishIcon} alt="svg" />
                 <h2>Popular Categories</h2>
             </div>
-            <Link to="">Get to know all our categories of products -</Link>
+            <Link to="">Get to know all our categories of products - </Link>
+           </div>
 
         </div>
     )
